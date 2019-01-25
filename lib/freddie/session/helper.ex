447fe64877c93f3ebe2d03619075ca8bfe5e2e97
Kernelgarden <kernelgarden@gmail.com>
2019-01-25 15:17:43 +0900

    prepare packet handling

--- lib/freddie/session/helper.ex
+++ lib/freddie/session/helper.ex
@@ -2,4 +2,8 @@ defmodule Freddie.Session.Helper do
   def activate_socket(socket) do
     :inet.setopts(socket, [active: :once])
   end
+
+  def pack_packet(header, payload) do
+
+  end
 end
---
+++ lib/freddie/session/packet_handler.ex
@@ -0,0 +1,6 @@
+defmodule Freddie.Session.PacketHandler do
+
+  def onRead() do
+
+  end
+end
